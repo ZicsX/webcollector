bfb781b52095d5df95debfb6f4e6b4a1014578de
Satish Mishra <zicsxone@gmail.com>
2023-08-22 04:13:02 +0530

    subprocess

--- webcollect/spiders/run_spider.py
+++ webcollect/spiders/run_spider.py
@@ -1,8 +1,12 @@
-from scrapy.crawler import CrawlerProcess
+import subprocess
 from scrapy.utils.project import get_project_settings
-from .async_spider import AsyncSpider
 
 def run_spider(url):
-    process = CrawlerProcess(get_project_settings())
-    process.crawl(AsyncSpider, url=url)
-    process.start()
+    spider_name = "async_spider"
+    cmd = ['scrapy', 'crawl', spider_name, '-a', f'url={url}']
+    
+    try:
+        result = subprocess.check_output(cmd, stderr=subprocess.STDOUT)
+        print(result.decode('utf-8'))
+    except subprocess.CalledProcessError as e:
+        print("Error occurred:", e.output.decode('utf-8'))
