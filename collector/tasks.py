2e7323745ae5ae4e52f9decfb5036abe9aac0b69
Satish Mishra <zicsxone@gmail.com>
2023-08-27 06:44:25 +0530

    create celery task using subprocess

--- collector/tasks.py
+++ collector/tasks.py
@@ -1,13 +1,14 @@
-from __future__ import absolute_import, unicode_literals
 from celery import shared_task
-from webcollect.spiders.run_spider import run_spider
+from urllib.parse import urlparse
 from collector.models import DomainCrawl
+import subprocess
 
 @shared_task
-def scrape_website(domain, hash_value):
-    url = f'https://{domain}'
-    run_spider(url)
-    
+def run_spider(target_url, hash_value):
+    domain_name = urlparse(target_url).netloc
+
+    subprocess.run(['scrapy', 'crawl', 'website_crawler', '-a', f'target_url={target_url}', '-s', f'JOBDIR=jobs/{domain_name}'])
+
     # Update the status to COMPLETED once crawling finishes
     crawl_instance = DomainCrawl.objects.get(hash_value=hash_value)
     crawl_instance.status = 'COMPLETED'
